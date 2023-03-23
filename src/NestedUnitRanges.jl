8fdeb52e6bcde89e73d5c78d0a5b21e704ff9595
Kristof Cools <krcools@gmail.com>
2023-03-23 18:20:22 +0100

    nested unit ranges implement block axis

--- src/NestedUnitRanges.jl
+++ src/NestedUnitRanges.jl
@@ -1,5 +1,110 @@
 module NestedUnitRanges
 
-# Write your package code here.
+import BlockArrays
+import ArrayLayouts
+import AbstractTrees
 
+export nestedrange
+
+struct NestedUnitRange <: AbstractUnitRange{Int}
+    length::Int
+    first::Int
+    children::Vector{NestedUnitRange}
+end
+
+"""
+    rebase_axis(nested_range)
+
+Return a deep copy of `nested_range` that is rebased such that the first entry equals `1`.
+"""
+function rebase_axis(nested_range, offset=1)
+    len = nested_range.length
+    rebased_children = NestedUnitRange[]
+    child_offset = 1
+    for child in nested_range.children
+        rebased_child = rebase_axis(child, child_offset)
+        push!(rebased_children, rebased_child)
+        child_offset += child.length
+    end
+    NestedUnitRange(len, offset, rebased_children)
+end
+
+Base.axes(r::NestedUnitRange) = (rebase_axis(r),)
+Base.first(r::NestedUnitRange) = r.first
+Base.last(r::NestedUnitRange) = r.first + r.length - 1
+
+"""
+s
+"""
+function nestedrange(tree, first=1, treesize=nestedsum)
+    children = Vector{NestedUnitRange}()
+    child_first = first
+    # tree_size = 0
+    for len in AbstractTrees.children(tree)
+        child = nestedrange(len, child_first, treesize)
+        child_first += child.length
+        push!(children, child)
+    end
+    return NestedUnitRange(treesize(tree), first, children)
+end
+
+
+function nestedsum(lengths)
+    total = 0
+    for len in lengths
+        total += nestedsum(len)
+    end
+    return total
+end
+nestedsum(lengths::Int) = lengths
+
+
+# Implement the blockaxes interface
+function BlockArrays.blockaxes(ax::NestedUnitRange)
+    if isempty(ax.children)
+        return (Base.OneTo(1),)
+    end
+    BlockArrays.blockaxes(BlockArrays.blockedrange([ch.length for ch in ax.children]))
+end
+
+
+function BlockArrays.getindex(ax::NestedUnitRange, K::BlockArrays.Block{1})
+    if isempty(ax.children)
+        @assert K == BlockArrays.Block(1)
+        return ax
+    end
+    return ax.children[K.n[1]]
+end
+
+
+function BlockArrays.getindex(ax::NestedUnitRange, bs::BlockArrays.BlockSlice)
+    getindex(ax, bs.block)
+end
+
+
+function BlockArrays.blockfirsts(ax::NestedUnitRange)
+    isempty(ax.children) && return Int[1]
+    Int[ch.first for ch in ax.children]
+end
+
+
+function BlockArrays.blocklasts(ax::NestedUnitRange)
+    isempty(ax.children) && return Int[ax.first + ax.length - 1]
+
+    lasts = Int[]
+    last = ax.first-1
+    for child in ax.children
+        last += child.length
+        push!(lasts, last)
+    end
+    return lasts
+end
+
+
+function BlockArrays.findblock(ax::NestedUnitRange, k)
+    ax1 = blockedrange([ch.length for ch in ax.children])
+    findblock(ax1, k)
+end
+
+ArrayLayouts.sub_materialize(::Any, V, axs::Tuple{<:NestedUnitRange}) = BlockArrays.PseudoBlockArray(Array(V), axs)
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,53 @@
 using NestedUnitRanges
 using Test
 
-@testset "NestedUnitRanges.jl" begin
-    # Write your tests here.
+import BlockArrays
+
+@testset "NestedUnitRange.jl" begin
+    nur = nestedrange([[2,3],4])
+
+    @test length(nur) == 9
+    @test BlockArrays.blocksize(nur) == (2,)
+
+    data = rand(9)
+    pbv = BlockArrays.PseudoBlockVector(data, (nur,))
+    @test axes(pbv) == (nur,)
+
+    v1 = view(pbv, BlockArrays.Block(1))
+    @test v1 isa SubArray
+    @test axes(v1,1) == nestedrange([2,3])
+
+    v12 = view(v1, BlockArrays.Block(2))
+    @test v12 isa SubArray
+    @test axes(v12,1) == nestedrange([3])
+
+    v12[end] = 123
+    @test pbv[5] == 123
+
+    a1 = getindex(pbv, BlockArrays.Block(1))
+    @test a1 isa BlockArrays.PseudoBlockVector
+    @test axes(a1,1) == nestedrange([2,3])
+    
+    a2 = getindex(pbv, BlockArrays.Block(2))
+    @test a2 isa BlockArrays.PseudoBlockVector
+    @test axes(a2,1) == nestedrange([4])
+
+    a12 = getindex(a1, BlockArrays.Block(1))
+    @test a12 isa BlockArrays.PseudoBlockVector
+    @test axes(a12,1) == nestedrange([2])
+
+    a22 = getindex(a1, BlockArrays.Block(2))
+    @test a22 isa BlockArrays.PseudoBlockVector
+    @test axes(a22,1) == nestedrange([3])
+
+    a12[end] = 321
+    @test pbv[5] == 123
+
+    w12 = view(a1, BlockArrays.Block(2))
+    @test w12 isa SubArray
+    @test axes(w12,1) == nestedrange([3])
+
+    b12 = getindex(w12, BlockArrays.Block(1))
+    @test b12 isa BlockArrays.PseudoBlockVector
+    @test axes(b12,1) == nestedrange([3])
 end
